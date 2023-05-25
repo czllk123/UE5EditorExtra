c341b8983f5154863549de204f60b3a3e0f218de
czllk123 <970111047@qq.com>
2023-05-26 02:43:17 +0800

    feat:自定义testcomponent &&添加layer层

---
+++ Plugins/LinkExtra/Source/LinkExtra/Private/TestComponent/TestComponent.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestComponent.h"
+
+
+// Sets default values for this component's properties
+UTestComponent::UTestComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	
+	//“允许这个组件在每一帧都被tick”。
+	PrimaryComponentTick.bCanEverTick = true;
+
+	//在构造函数中，设置头文件声明的属性的默认值
+	TestBoolean = true;
+	TestFloat = 1.0f;
+	TestInt = 100;
+}
+
+
+// Called when the game starts
+void UTestComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// 初始化LastFrameBoolean
+	LastFrameBoolean = TestBoolean;
+	
+}
+
+
+// Called every frame
+void UTestComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	//Super代表父类，可以添加自定义行为，然后保留父类的行为
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// 检查TestBoolean是否有变化
+	if(TestBoolean!=LastFrameBoolean)
+	{
+		//如果TestBoolean改变了，打印一串字符
+		UE_LOG(LogTemp,Warning,TEXT("TestBoolean has changed!"));
+
+		//更新LastFrameBoolean
+		LastFrameBoolean = TestBoolean;
+	}
+}
+
---
+++ Plugins/LinkExtra/Source/LinkExtra/Private/TestComponent/TestComponent.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TestComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LINKEXTRA_API UTestComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UTestComponent();
+	//EditAnywhere允许你在编辑器中任何地方查看和编辑这个属性。
+	//BlueprintReadWrite允许你在蓝图中获取和设置这个属性。
+	//Category = "My Category"在编辑器的详细面板中将这个属性归类到"My Category"分类中。
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Parameters",DisplayName="布尔值")
+	bool TestBoolean;
+
+	//这是一个浮点数
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Parameters",DisplayName="浮点数")
+	float TestFloat;
+
+	//这是一个整形
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parameters",DisplayName="整形")
+	int TestInt;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	bool LastFrameBoolean;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
