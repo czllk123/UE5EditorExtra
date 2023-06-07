93fb8f5dc94ce8fea3a44e36db59047735c100dd
czllk123 <970111047@qq.com>
2023-06-08 02:53:41 +0800

    feat：新建Species Uobject class

---
+++ Plugins/LinkExtra/Source/LinkExtra/Private/FoliageTools/Species.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Species.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINKEXTRA_API USpecies : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category="Parameters", DisplayName="FoliageTypes")
+	TArray<UFoliageType*>FoliageTypes;
+
+	UPROPERTY(EditAnywhere, Category="Parameters", DisplayName="权重")
+	float Weight = 1.0f;
+};
